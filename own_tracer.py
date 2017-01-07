d896136bf3f174c4cfd90aabee1762a7f83866aa
manofdalmasca <redstern13@yahoo.com>
2017-01-07 16:13:22 -0500

    Add files via upload
    Skeleton code for the project. Not necessarily exhaustive.

---
+++ own_tracer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env/ python3 
+#to write a personalized traceroute program, meant to report on 
+#the routes a packet takes to a given IP destination. This will be 
+#accomplished by use of Echo Requests. See RFC 791 and RFC 792. 
+
+#def outbound():
+#python code for sending packets in general, involving sockets
+#will take parameters for the destination and the content 
+
+#def inbound(): 
+#python code for receiving packets in general, involving sockets 
+#will ostensibly take a timeout-value parameter, if it's not to wait forever
+
+#def makeIPHeader(): 
+#code for building an IP header
+
+#def makeICMPHeader(): 
+#code for building an ICMP header
+
+#def compilePacket(): 
+#code for building the whole packet, complete with headers 
+
+#def iterations(): 
+#code for iteratively sending out packets with higher TTL's, and 
+#receiving the responses 
+
+#if __name__ == '__main__': 
+#  iterations()
