f2526c1062166ef39e82b0dd9bcc8afdb89f6bfe
manofdalmasca <redstern13@yahoo.com>
2017-01-07 22:36:01 -0500

    hi
    This version has tentative code for building IP and ICMP headers, and as before, the tentative code for creating sending and receiving sockets. Commented; as yet untested.

--- own_tracer.py
+++ own_tracer.py
@@ -43,11 +43,72 @@ def outbound(shipment, endereco):
     return "***" #we only return a default value and move on 
 
 
-#def makeIPHeader(): 
-#code for building an IP header
+def makeIPHeader(slouchTowards, slouchFrom, slouchID, expirationDate): 
+#code for building an IP header, to be returned in the form of packed 
+#binary data 
+  #For IPv4 and a 5-word header: '0100' + '0101' = '01000101' = 69
+  versionAndIHL = 69 
+  serviceType = 0 #we don't need precedence or extra throughput, and 
+  #we'll be implementing our own form of reliability. What's more, there's 
+  #no guarantee that the en-route networks will even do anything special 
+  #if these things are requested. So as not to request them, we'll 
+  #leave [serviceType] as 0 (see RFC 791). 
+  totalLength = 28 #in terms of bytes: 20 for the IP header, 8 for the ICMP 
+  #header, and nothing after that (at least not for outbound packets).
+  #and we'll get a unique ID from the calling context, as [slouchID]. 
+  flags_n_frags = 0 #may_fragment, is_last_fragment, and is_first_fragment
+  #all understood from the mere number 0, as per RFC 791 
+  #we also get a time to live from the calling context, as [expirationDate]. 
+  protocolVal = 1 #for ICMP, as per RFCs 791 and 792 
+  checkSum = 0 #not what the final checksum will be, but with any luck, 
+  #that won't be our problem 
+  #and we get our source and destination from the calling context 
 
-#def makeICMPHeader(): 
+  #The following section will build (most of) the bitstring representing 
+  #our IP header. See the documentation for Python's [struct] module for 
+  #an explanation of what follows. 
+  ip_header = struct.pack('!BBHHHBBH', 
+  versionAndIHL, 
+  serviceType, 
+  totalLength, 
+  slouchID, 
+  flags_n_frags, 
+  expirationDate, 
+  protocolVal, 
+  checkSum) 
+
+  #It remains to tack on the source and destination variables, which 
+  #will be given in a slightly different format than the other header fields
+  #(which is why we can't just [struct.pack] them). The useful part is that 
+  #bitstrings can be concatenated just like regular strings, at least in 
+  #python. 
+  ip_header = ip_header + socket.inet_aton(slouchFrom) #source 
+  ip_header = ip_header + socket.inet_aton(slouchTowards) #destination 
+  #and we are done 
+  return ip_header
+   
+
+
+#def makeICMPHeader(controlID, sequenceCount): 
 #code for building an ICMP header
+  typeVal = 8 #for an Echo Request, as per RFC 792 
+  codeVal = 0 #irrelevant in this context; set to 0 as per RFC 792 
+  checker = 0 #to begin with, anyway 
+  #and the calling context gives us an ID number for this ICMP call 
+  #as well as a sequence number 
+
+  #What follows will build the bitstring representing our ICMP header. 
+  #See the Python documentation on [struct.pack] for an explanation of what 
+  #follows. 
+  icmp_header = struct.pack('!BBHHH', 
+  typeVal, 
+  codeVal, 
+  checker, 
+  controlID, 
+  sequenceCount) 
+
+  return icmp_header 
+  
 
 #def compilePacket(): 
 #code for building the whole packet, complete with headers 
